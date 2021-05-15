a112b08650b73b2b0006f0c99f554dba5cd6f4f2
NickDeWyngaert <43341508+NickDeWyngaert@users.noreply.github.com>
2021-05-15 14:34:23 +0200

    Basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { 
+    path: '', 
+    component: UserComponent 
+  },
+  { 
+    path: 'admin', 
+    component: AdminComponent 
+  },
+  { 
+    path: '**', 
+    component: PageNotFoundComponent 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
